118aed4715c8da35681b8d5428b6574a186b0b3f
Fabien-B <fabien.bonneval@gmail.com>
2021-11-29 23:42:05 +0100

    Take links from command-line arguments.

--- src/main.rs
+++ src/main.rs
@@ -5,52 +5,66 @@ mod transport;
 mod serial_link;
 mod udp_link;
 
-use std::sync::mpsc;
-use std::{thread, time::Duration};
-use serial_link::{SerialLinkConfig};
+use std::{thread, time::Duration, sync::mpsc};
+use anyhow::Result;
+use serial_link::{SerialLinkConfig, SerialLink};
 use udp_link::UdpLink;
-
-use link::Link;
-
-use crate::link::Message;
+use link::{Link, Message};
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
+use anyhow::anyhow;
+use clap::{Arg, App};
 
 
-const PORT1: &str = "/dev/ttyUSB0";
-const UDP_SERVER: &str = "127.0.0.1:3456";
+fn main() -> Result<()> {
 
-fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+
+    let matches = App::new("Serduck")
+        .version("0.1.0")
+        .about("UDP/Serial message server.")
+        .arg(Arg::with_name("serial")
+                 .short("s")
+                 .long("serial")
+                 .takes_value(true)
+                 .help("<port>:<baudrate>"))
+        .arg(Arg::with_name("udp")
+                 .short("u")
+                 .long("udp")
+                 .takes_value(true)
+                 .help("<addr>:<port>"))
+        .get_matches();
 
     let mut links: Vec<Box<dyn Link>> = Vec::new();
     let mut errors : Vec<String> = Vec::new();
 
 
     let (tx, rx) = mpsc::channel::<Message>();
+    
 
-
-    match UdpLink::new(UDP_SERVER, tx.clone(), 1) {
-        Ok(ul) => links.push(Box::new(ul)),
-        Err(e) => {
-            let msg = format!("Error for {}: {:?}", PORT1, e);
-            println!("{}", msg);
-            errors.push(msg);
-        },
+    if let Some(serials) = matches.values_of("serial") {
+        for ser in serials {
+            match add_serial(ser, tx.clone()) {
+                Ok(sl) => links.push(Box::new(sl)),
+                Err(e) => errors.push(e.to_string())
+            }
+        }
     }
 
-    let slc = SerialLinkConfig {
-        port:PORT1.into(),
-        baudrate: 57600,
-        timeout: 1,
-    };
+    if let Some(udps) = matches.values_of("udp") {
+        for udp in udps {
+            match add_udp(udp, tx.clone()) {
+                Ok(ul) => links.push(Box::new(ul)),
+                Err(e) => errors.push(e.to_string())
+            }
+        }
+    }
 
-    match slc.start(tx.clone()) {
-        Ok(sl) => links.push(Box::new(sl)),
-        Err(e) => {
-            let msg = format!("Error for {}: {:?}", PORT1, e);
-            println!("{}", msg);
-            errors.push(msg);
-        },
+    for err in errors {
+        println!("{}", err);
+    }
+
+    if links.len() == 0 {
+        return Err(anyhow!("No link successfully started!"))
     }
 
     let term = Arc::new(AtomicBool::new(false));
@@ -72,3 +86,35 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+
+fn add_serial(serial_params: &str, tx: mpsc::Sender<Message>) -> Result<SerialLink> {
+    let params: Vec<_> = serial_params.split(':').collect();
+    if params.len() == 2 {
+        let port = params[0];
+        let baudrate: u32 = params[1].parse()?;
+        let slc = SerialLinkConfig {
+            port: port.into(),
+            baudrate,
+            timeout: 1,
+        };
+
+        match slc.start(tx) {
+            Ok(sl) => Ok(sl),
+            Err(e) => {
+                Err(anyhow!("Error for {} : {:?}", serial_params, e))
+            },
+        }
+
+    } else {
+        Err(anyhow!("can't split \"{}\" as port:baudrate", serial_params))
+    }
+}
+
+fn add_udp(udp_params: &str, tx: mpsc::Sender<Message>) -> Result<UdpLink> {
+    match UdpLink::new(udp_params, tx, 1) {
+        Ok(ul) => Ok(ul),
+        Err(e) => {
+            Err(anyhow!("Error for {} : {:?}", udp_params, e))
+        },
+    }
+}
